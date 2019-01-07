8735da58e81ee0b00200f9edd19adf82f2b7ed79
mauritscottyn <34495991+mauritscottyn@users.noreply.github.com>
2019-01-07 10:23:43 +0100

    added exercise two_sum

--- two_sum.py
+++ two_sum.py
@@ -1,2 +1,13 @@
-def two_sum(x : list):
-    
+def two_sum(nums : list, target : int):
+    result = []
+    for i in range(0, len(nums)):
+        for k in range(i+1, len(nums)):
+            if nums[i] + nums[k] == target:
+                if len(result) < 2:
+                    if nums[i] == nums[k]:
+                        result.append(nums.index(nums[i]))
+                        result.append(nums.index(nums[k], nums.index(nums[i]) + 1))
+                    else:
+                        result.append(nums.index(nums[i]))
+                        result.append(nums.index(nums[k]))
+    return result
